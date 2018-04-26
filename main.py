935b6adf06f8ba161398ebae7fc5ca9d324f945a
mcheng3 <michael.cheng65@gmail.com>
2018-04-25 22:50:37 -0400

    Finish

---
+++ main.py
@@ -0,0 +1,33 @@
+def has_chars(word, chars):
+        return any([x for x in word if x in chars])
+
+def meets_requirements(password):
+        return(has_chars(password, [chr(c) for c in range(ord('a'), ord('z') + 1)]) and 
+           has_chars(password, [chr(c) for c in range(ord('A'), ord('Z') + 1)]) and
+           has_chars(password, [chr(c) for c in range(ord('0'), ord('9') + 1)]))
+
+print meets_requirements('aA0');
+print meets_requirements('zZ9');
+print meets_requirements('z');
+print meets_requirements('zZ');
+
+def strength(password):
+	rating = 1
+	if has_chars(password, [chr(c) for c in range(ord('a'), ord('z') + 1)]):
+		rating += 1
+		if has_chars(password, [chr(c) for c in range(ord('A'), ord('Z') + 1)]):
+			rating += 2
+	elif has_chars(password, [chr(c) for c in range(ord('A'), ord('Z') + 1)]):
+			rating += 1
+	if has_chars(password, [chr(c) for c in range(ord('0'), ord('9') + 1)]):
+		rating += 3
+	if has_chars(password, ". ? ! & # , ; : - _ *".split()):
+		rating += 3
+	
+	return rating
+
+print strength("x")
+print strength("aZ")
+print strength("Z")
+print strength("aZ0")
+print strength("aZ0.")
